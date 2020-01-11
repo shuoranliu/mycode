9d0109f8f5695b0932a9d624d3c2c1e483d77f55
Shuoran Liu <shuoranliu@gmail.com>
2020-01-11 21:40:46 +0800

    recursive solution

--- q2/main.c
+++ q2/main.c
@@ -14,29 +14,84 @@ typedef struct data_struct_s
 
 map_t mymap;
 
-void parse_value(char *source)
-{
-    char *key = malloc(256);
-    char *value= malloc(256);
-    char *token= strtok(source, "=");
+int parse_token(char *source, int start, int end);
 
-    while (token != NULL) {
-        if (token[0] == '\[') {
-            token = strtok(NULL, "=");
-            strncpy(key, token+1, strlen(token));
-        } else {
-            strncpy(key, token, strlen(token));
-            token = strtok(NULL, ",");
-            if (token[0] == '\[') {
-                continue;
-            } else {
-                strncpy(value, token, strlen(token));
-            }
-            token = strtok(NULL, "=");
-        }
-        printf("key: %s, value: %s\n", key, value);
+void parse_value(char *source, int start, int end)
+{
+    int cursor = start;
+
+    //printf("start=%d, end=%d\n", start, end);
+    while (source[cursor] != '=' && cursor < end) {
+        cursor++;
     }
-    printf("key: %s, value: %s\n", key, value);
+
+//    char *key = malloc(cursor-start+1);
+//    snprintf(key, cursor-start+1, "%s", source+start);
+
+    char *value = malloc(end-cursor);
+    snprintf(value, end-cursor, "%s", source+cursor+1);
+
+//    printf("key=%s, value=%s\n", key, value);
+
+    if (value[0] == '[') {
+        int new_start = cursor+2;
+        int new_end = end - 1;
+
+        new_start = parse_token(source, new_start, new_end);
+        while (source[new_start] != '\0' && new_start < new_end) {
+            new_start = parse_token(source, new_start, new_end);
+        }
+    } else {
+        // add to hashmap
+        data_struct_t* object = malloc(sizeof(data_struct_t));
+        snprintf(object->key_string, cursor-start+1, "%s", source+start);
+        object->val = malloc(end-cursor);
+        snprintf((char *)object->val, end-cursor, "%s", source+cursor+1);
+        printf("key=%s, value=%s\n", object->key_string, (char *)object->val);
+        hashmap_put(mymap, object->key_string, object);
+    }
+
+    free(value);
+}
+
+int parse_token(char *source, int start, int end)
+{
+    int remain = start;
+    int array = 0;
+
+    while (source[remain] != '\0' && remain < end) {
+        if (source[remain] == ',' || source[remain] == '[' || source[remain] == ']') {
+            if (source[remain] == ',') {
+                if (array == 0) {
+                    break;
+                } else {
+                    remain++;
+                    continue;
+                }
+            }
+
+            if (source[remain] == '[') {
+                remain++;
+                array++;
+                continue;
+            }
+
+            remain++;
+            array--;
+            continue;
+        }
+
+        remain++;
+    }
+
+    if (remain == start)
+        return remain;
+
+    // parse value from start to remain
+    //
+    parse_value(source, start, remain);
+
+    return remain+1;
 }
 
 int main(void)
@@ -44,32 +99,20 @@ int main(void)
     data_struct_t* value;
     char key_string[KEY_MAX_LENGTH];
     int error;
+    int start = 0;
 
     char source[] = "key1=value1,key2=value2,key3=[key4=value4,key5=value5,key6=[key7=value7]],key8=value8";
+    //char source[] = "key6=[key7=value7],key8=value8";
 
     mymap = hashmap_new();
 
-    parse_value(source);
+    start = parse_token(source, start, strlen(source));
+    while (source[start] != '\0' && start < strlen(source)) {
+        start = parse_token(source, start, strlen(source));
+    }
 
-//    char *token = strtok(source, ",");
-//
-//    while (token != NULL) {
-//        printf("token: %s\n", token);
-//        token = strtok(NULL, ",");
-//    }
-
-    value = malloc(sizeof(data_struct_t));
-    snprintf(value->key_string, KEY_MAX_LENGTH, "%s", "key1");
-    value->val = "value1";
-
-
-    hashmap_put(mymap, value->key_string, value);
-
-    snprintf(key_string, KEY_MAX_LENGTH, "%s", "key1");
-    error = hashmap_get(mymap, key_string, (void**)(&value));
-
-    printf("value is %s\n", value->val);
-
-    free(value);
+    //FIXME: free object
     hashmap_free(mymap);
+
+    return 1;
 }
